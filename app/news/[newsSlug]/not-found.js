3581c43625a373483614cf15dacccb49a02ec1d4
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-12 16:03:22 +0000

    Add handling not found errors

---
+++ app/news/[newsSlug]/not-found.js
@@ -0,0 +1,10 @@
+function NewsNotFoundPage() {
+  return (
+    <div id="error">
+      <h1>News page is found!</h1>
+      <p>Unfortunately, we could not find requested article.</p>
+    </div>
+  );
+}
+
+export default NewsNotFoundPage;
---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Not found!</h1>
+      <p>The page you are looking for could not be found.</p>
+    </div>
+  );
+}
+
+export default NotFoundPage;
