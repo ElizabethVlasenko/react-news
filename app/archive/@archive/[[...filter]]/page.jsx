0f67b600b420183811fa4a2c62716a21124fb1cb
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-12 16:31:24 +0000

    Configure catch-all route

--- app/archive/@archive/[[...filter]]/page.jsx
+++ app/archive/@archive/[[...filter]]/page.jsx
@@ -1,8 +1,16 @@
 import { getAvailableNewsYears } from "@/app/_lib/news";
 import Link from "next/link";
 
-function page() {
+//[[...filter]] - catch-all; will be activated after any path segments after archive;
+//it catches  "/archive/"" so it was conflicting with the @archive/page.js file
+
+function page({ params }) {
+  const newsYear = params.filter; // returns an array of the path [ '2021', '2' ]
   const links = getAvailableNewsYears();
+
+  // const news = getNewsForYear(newsYear);
+
+  console.log(newsYear);
 
   return (
     <header id="archive-header">
