6abab867522f3dc320b884b7b279275c081a0c9c
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-13 15:23:50 +0000

    Refactor the Main header component

--- app/_components/MainHeader.jsx
+++ app/_components/MainHeader.jsx
@@ -1,10 +1,7 @@
-"use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import NavLink from "./NavLink";
 
 function MainHeader() {
-  const path = usePathname();
-
   return (
     <header id="main-header">
       <div id="logo">
@@ -12,15 +9,12 @@ function MainHeader() {
       </div>
       <nav>
         <ul>
-          <Link href="/" className={path === "/" ? "active" : ""}>
-            Home
-          </Link>
-          <Link
-            href="/news"
-            className={path.startsWith("/news") ? `active` : null}
-          >
-            News
-          </Link>
+          <li>
+            <NavLink href="/news">News</NavLink>
+          </li>
+          <li>
+            <NavLink href="/archive">Archive</NavLink>
+          </li>
         </ul>
       </nav>
     </header>
