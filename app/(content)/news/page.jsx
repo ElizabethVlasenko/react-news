5f36dae7c19f32b80ddd8f61cb64b5380242d855
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-11-13 16:46:44 +0000

    Add fetching data from a backend

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,12 +1,18 @@
-import { DUMMY_NEWS } from "@/dummy-news";
-
 import NewsList from "../../_components/NewsList";
 
-function Page() {
+async function Page() {
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Something went wrong!");
+  }
+
+  const news = await response.json();
+
   return (
     <>
       <h1>News page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      <NewsList news={news} />
     </>
   );
 }
